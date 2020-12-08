52834fbf730af04c97fb17b7b21887ca78bd4bee
Ahriana <rosiecode@outlook.com>
2020-12-08 21:23:20 +0000

    show first dart for songs first note

--- TimingAttack/Class1.cs
+++ TimingAttack/Class1.cs
@@ -2,44 +2,48 @@
 using MelonLoader;
 using System;
 
-namespace TimingAttack
-{
-    public class TimingAttackClass : MelonMod
-    {
-        public static bool ForceEnable = false;
+namespace TimingAttack {
+	public class TimingAttackClass: MelonMod {
+		public static bool ForceEnable = false;
 
-        public override void OnApplicationStart() {
-            Config.RegisterConfig();
-        }
+		public override void OnApplicationStart () {
+			Config.RegisterConfig();
+		}
 
-        public override void OnModSettingsApplied() {
-            Config.OnModSettingsApplied();
-        }
+		public override void OnModSettingsApplied () {
+			Config.OnModSettingsApplied();
+		}
 
-        [HarmonyPatch(typeof(CueDartManager), "ShouldCreateDart")]
-        class CueDartManager_ShouldCreateDart {
-            static void Postfix(ref bool __result) {
-                if (Config.HiddenDarts == true || ForceEnable == true) { __result = false; }
-            }
-        }
+		[HarmonyPatch(typeof(AudioDriver), "StartPlaying")]
+		class AudioDriver_StartPlaying {
+			static void Prefix () {
+				Data.firstDart = true;
+			}
+		}
 
-        [HarmonyPatch(typeof(CueDart), "SetShowing")]
-        class CueDart_SetShowing {
-            static void Postfix(ref bool showing) {
-                if (Config.HiddenDarts == true || ForceEnable == true) { showing = false; }
-            }
-        }
+		[HarmonyPatch(typeof(CueDartManager), "ShouldCreateDart")]
+		class CueDartManager_ShouldCreateDart {
+			static void Postfix (ref bool __result) {
+				if (Config.HiddenDarts == true || ForceEnable == true || Data.firstDart == true) {
+					if (Data.firstDart == true) {
+						Data.firstDart = false;
+						__result = true;
+					} else {
+						__result = false;
+					}
+				}
+			}
+		}
 
-        [HarmonyPatch(typeof(Telegraph), "Init", new Type[] { typeof(SongCues.Cue), typeof(float) })]
-        private static class PatchInit {
-            private static void Postfix(Telegraph __instance, SongCues.Cue cue, float animationSpeed) {
-                if (Config.HiddenClouds == true || ForceEnable == true) {
-                    if (cue.behavior == Target.TargetBehavior.Melee || cue.behavior == Target.TargetBehavior.Dodge) { return; } 
-                    // __instance.circleMesh.enabled = false;
-                    __instance.cloud.enabled = false;
-                }
-            }
-        }
-    }
+		[HarmonyPatch(typeof(Telegraph), "Init", new Type[] { typeof(SongCues.Cue), typeof(float) })]
+		private static class PatchInit {
+			private static void Postfix (Telegraph __instance, SongCues.Cue cue, float animationSpeed) {
+				if (Config.HiddenClouds == true || ForceEnable == true) {
+					if (cue.behavior == Target.TargetBehavior.Melee || cue.behavior == Target.TargetBehavior.Dodge) { return; }
+					__instance.cloud.enabled = false;
+				}
+			}
+		}
+	}
 }
 
